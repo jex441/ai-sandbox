5df9e59a274b122218f9892fb143daa81e122a92
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-02-20 17:16:56 -0500

    excess code removed

--- langchain/6-agents/app.py
+++ langchain/6-agents/app.py
@@ -2,22 +2,15 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
-
-import getpass
-import os
-
-if not os.environ.get("OPENAI_API_KEY"):
-  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
-
 from langchain.chat_models import init_chat_model
 
 model = init_chat_model("gpt-4", model_provider="openai")
 
-# Create the agent
 memory = MemorySaver()
 search = TavilySearchResults(max_results=2)
 tools = [search]
 
+# Create the agent
 agent_executor = create_react_agent(model, tools, checkpointer=memory)
 
 config = {"configurable": {"thread_id": "abc123"}}
@@ -30,9 +23,9 @@ for step, metadata in agent_executor.stream(
     stream_mode="messages",
 ):
     if metadata["langgraph_node"] == "agent" and (text := step.text()):
-        print(text, end="|")
+        print(text)
 
-user_input_2 = input("Enter your follow up query: ")
+user_input_2 = input("Enter your follow up query to test memory: ")
 
 for step, metadata in agent_executor.stream(
     {"messages": [HumanMessage(content=user_input_2)]},
@@ -40,4 +33,4 @@ for step, metadata in agent_executor.stream(
     stream_mode="messages",
 ):
     if metadata["langgraph_node"] == "agent" and (text := step.text()):
-        print(text, end="|")
+        print(text)

c07539e76a0fdf372852a4e08411cde71dbdecf2
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-02-20 17:54:19 -0500

    simple translate

--- langchain/1-prompts/app.py
+++ langchain/1-prompts/app.py
@@ -1,13 +1,17 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
-messages = [
-    SystemMessage("Translate the following from English into Japanese"),
-    HumanMessage("hi! how are you?"),
-]
+system_template = "Translate the following from English into {language}"
 
-response = model.invoke(messages)
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+
+response = model.invoke(prompt)
 
 print(response.content)

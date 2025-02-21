298b777122bf46c3ebb05d64d44b7f27d5bfc303
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-02-21 18:34:13 -0500

    retriever rm

--- langchain/2-retriever/app.py
+++ langchain/2-retriever/app.py
@@ -2,10 +2,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_openai import OpenAIEmbeddings
-from typing import List
-
-from langchain_core.documents import Document
-from langchain_core.runnables import chain
 
 file_path = "../../nke-10k-2023.pdf"
 
@@ -26,14 +22,9 @@ vector_store = InMemoryVectorStore(embeddings)
 
 ids = vector_store.add_documents(documents=all_splits)
 
-retriever = vector_store.as_retriever(
-    search_type="similarity",
-    search_kwargs={"k": 1},
+# Perform a similarity search
+results = vector_store.similarity_search_with_score(
+    "What was Nike's revenue in 2023?"
 )
 
-retriever.batch(
-    [
-        "How many distribution centers does Nike have in the US?",
-        "When was Nike incorporated?",
-    ],
-)
+print(results[0])
